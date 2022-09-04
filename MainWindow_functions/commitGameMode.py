30e5f789e4511326a8f07f492b01603c0f62fe1c
VvLloyd <francis.gemme@gmail.com>
2022-09-04 13:42:36 -0400

    adding icon

--- MainWindow_functions/commitGameMode.py
+++ MainWindow_functions/commitGameMode.py
@@ -1,9 +1,14 @@
 
 def commitGameMode(MainWindow):
 
-    #MainWindow.populateGMode(MainWindow.SelectedGameMode)
-    MainWindow.commitedGameMode = MainWindow.SelectedGameMode
-    print('Game Mode is now set')
+    selection = MainWindow.subWin.mode_var.get()
+
+    MainWindow.CommitGameMode = eval("["+str(selection)+", MainWindow.DoubleInMode]")
+
+    print('Game Mode is now set:')
+    print(MainWindow.CommitGameMode)
+    print(MainWindow.SelectedGameMode)
+
     MainWindow.destroySubWin()
 
     return
