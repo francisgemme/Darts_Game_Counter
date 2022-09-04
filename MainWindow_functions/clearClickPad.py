37081a7c809934f320b47fd154a03654481b9eaa
VvLloyd <francis.gemme@gmail.com>
2022-09-04 11:03:30 -0400

    adding icon

--- MainWindow_functions/clearClickPad.py
+++ MainWindow_functions/clearClickPad.py
@@ -3,16 +3,24 @@ from tkinter import *
 def clearClickPad(MainWindow):
     # A class "Game" with its properties must be sent to this function
     # Transfer created player to the Lobby
-    if MainWindow.editScoreMode == False:
-        MainWindow.input_Score.config(state=NORMAL)
-        MainWindow.input_Score.delete(0, END)
-        MainWindow.input_Score.config(state=DISABLED)
-        MainWindow.button_commitScore.configure(state=DISABLED)  # Clearing is also disabling the commit score button
+    currentPlayer = MainWindow.playerIndex[:1]  # Who is playing
 
-    if MainWindow.editScoreMode == True:
-        currentPlayer = MainWindow.playerIndex[:1]
-        eval("MainWindow.player_" + str(currentPlayer[0]) + "_label_2.config(state=NORMAL)")
-        eval("MainWindow.player_" + str(currentPlayer[0]) + "_label_2.delete(0, END)")
-        eval("MainWindow.player_" + str(currentPlayer[0]) + "_label_2.config(state=DISABLED)")
+    if MainWindow.DoubleInMode == True:
+        DoubleInStatus = eval("MainWindow.p"+str(currentPlayer[0])+"_DoubleInVar.get()")
+    else:
+        DoubleInStatus = False
+
+    if DoubleInStatus == True or MainWindow.DoubleInMode == False:
+        if MainWindow.editScoreMode == False:
+            MainWindow.input_Score.config(state=NORMAL)
+            MainWindow.input_Score.delete(0, END)
+            MainWindow.input_Score.config(state=DISABLED)
+            MainWindow.button_commitScore.configure(state=DISABLED)  # Clearing is also disabling the commit score button
+
+        if MainWindow.editScoreMode == True:
+            eval("MainWindow.player_" + str(currentPlayer[0]) + "_label_2.config(state=NORMAL)")
+            eval("MainWindow.player_" + str(currentPlayer[0]) + "_label_2.delete(0, END)")
+            eval("MainWindow.player_" + str(currentPlayer[0]) + "_label_2.config(state=DISABLED)")
+
         MainWindow.button_commitScore.configure(state=DISABLED)
     return
