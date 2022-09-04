d2f3459c82c78fd569e47653322975f6bf066030
VvLloyd <francis.gemme@gmail.com>
2022-09-04 13:42:55 -0400

    adding icon

--- MainWindow_functions/endTurn.py
+++ MainWindow_functions/endTurn.py
@@ -13,24 +13,43 @@ def endTurn(MainWindow):
         if currentPlayer < totalPlayer:
             if currentPlayer == [1]: # if player #1 turn
                 MainWindow.playerIndex[0] = 2
+                if MainWindow.DoubleInMode == True:
+                    MainWindow.player_1_checkbox.config(state=DISABLED)
+                    if MainWindow.p1_DoubleInVar.get() == True:
+                        MainWindow.player_1_checkbox.config(state=DISABLED)
+                    if MainWindow.p2_DoubleInVar.get() == False:
+                        MainWindow.player_2_checkbox.config(state=NORMAL)
             if currentPlayer == [2]:
                 MainWindow.playerIndex[0] = 3
+                if MainWindow.DoubleInMode == True:
+                    MainWindow.player_2_checkbox.config(state=DISABLED)
+                    if MainWindow.p2_DoubleInVar.get() == True:
+                        MainWindow.player_2_checkbox.config(state=DISABLED)
+                    if MainWindow.p3_DoubleInVar.get()== False:
+                        MainWindow.player_3_checkbox.config(state=NORMAL)
             if currentPlayer == [3]:
                 MainWindow.playerIndex[0] = 4
-            if currentPlayer == [4]:
-                MainWindow.playerIndex[0] = 1
+                if MainWindow.DoubleInMode == True:
+                    MainWindow.player_3_checkbox.config(state=DISABLED)
+                    if MainWindow.p3_DoubleInVar.get() == True:
+                        MainWindow.player_3_checkbox.config(state=DISABLED)
+                    if MainWindow.p4_DoubleInVar.get()== False:
+                        MainWindow.player_4_checkbox.config(state=NORMAL)
 
         if currentPlayer == totalPlayer:
             MainWindow.playerIndex[0] = 1
+            if MainWindow.DoubleInMode == True:
+                eval("MainWindow.player_"+str(currentPlayer[0])+"_checkbox.config(state=DISABLED)")
+                if eval("MainWindow.p"+str(currentPlayer[0])+"_DoubleInVar.get()") == True:
+                    eval("MainWindow.player_"+str(currentPlayer[0])+"_checkbox.config(state=DISABLED)")
+                if MainWindow.p1_DoubleInVar.get() == False:
+                    MainWindow.player_1_checkbox.config(state=NORMAL)
             #if this is the last player to play, the log "game turn" is updated by 1  **To be removed...
-
-        if currentPlayer == totalPlayer:
             MainWindow.currentGameTurn += 1
-            #toAppend = pd.DataFrame([[currentGameTurn]], columns=['gameTurn'])
-            #self.turnIndexLog = pd.concat([self.turnIndexLog, toAppend])
 
         MainWindow.arrowImage.grid_forget()
         MainWindow.button_endTurn.configure(state='normal')
+        MainWindow.updateIndexLog()
         MainWindow.refreshImages()
 
     return
